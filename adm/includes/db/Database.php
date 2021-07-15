414a5aaef1e516e00bec15ab1a46497f55381c8b
Wolaci <wolacijose8@gmail.com>
2021-07-14 22:11:43 -0300

    Configurando classe Database

--- adm/includes/db/Database.php
+++ adm/includes/db/Database.php
@@ -1,5 +1,15 @@
 <?php
 
+namespace includes\Db\Database;
+use \PDO;
+use \PDOException;
+
 class Database{
-  
+  const HOST = 'localhost';
+  const NAME = 'adm_sistema';
+  const USER = 'root';
+  const PASS = '';
+  private $table;
+  private $connection;
+
 }
