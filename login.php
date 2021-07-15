60be3766d06dfdbe1283e01e6352611abcfa74f1
Wolaci <wolacijose8@gmail.com>
2021-07-15 20:18:11 -0300

    Criando template e adicionando no login

--- login.php
+++ login.php
@@ -1,18 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Adm Sistema | Login</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="assets/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="assets/css/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="assets/css/adminlte.min.css">
+  <?php require_once('templates/templateChamada.php') ?>
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
@@ -62,12 +54,5 @@
   </div>
 </div>
 <!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="assets/js/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="assets/js/adminlte.min.js"></script>
 </body>
 </html>
