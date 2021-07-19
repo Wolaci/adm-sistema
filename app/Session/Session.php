657d33156689003066253dea8f0a0beff8fc6bcb
Wolaci <wolacijose8@gmail.com>
2021-07-19 17:28:43 -0300

    Criando classe de Session

---
+++ app/Session/Session.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Session;
+
+class Login{
+
+  private static function init(){
+    if(session_status() !== PHP_SESSION_ACTIVE){
+      session_start();
+    }
+  }
+
+  public static function getUserLogged(){
+    self::init();
+
+    return self::isLogged() ? $_SESSION['user'] : null;
+  }
+
+  public static function login($obUsusario){
+    self::init();
+    $_SESSION['user'] = [
+      'id'=>$obUsusario->id,
+      'name'=>$obUsusario->name,
+      'email'=>$obUsusario->email
+    ];
+
+    header('location: index.php');
+    exit;
+  }
+
+  public static function logout(){
+    self::init();
+    unset($_SESSION['user']);
+
+    header('location: login.php');
+    exit;
+  }
+
+  public static function isLogged(){
+    self::init();
+    return isset($_SESSION['user']['id']);
+  }
+
+  public static function requireLogin(){
+    if(!self::isLogged()){
+      header('location: login.php');
+      exit;
+    }
+  }
+
+  public static function requireLogout(){
+    if(self::isLogged()){
+      header('location: index.php');
+      exit;
+    }
+  }
+
+}
