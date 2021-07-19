5ac745d6b13b46a15657ffb5de0510f040b01d92
Wolaci <wolacijose8@gmail.com>
2021-07-19 17:19:05 -0300

    Criando método cadastrar usuário

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -4,53 +4,29 @@ namespace App\Entity;
 use \App\Db\Database;
 
 class User{
+  
   public $id;
   public $name;
   public $email;
+  public $password;
   public $cpf;
-  public $telefone;
+  public $phone;
   public $status;
 
-  public static function getUsers($where=null, $order=null, $limit = null){
-    return (new Database('users'))->select($where, $order, $limit)
-    ->fetchAll(PDO::FETCH_CLASS,self::class);
-  }
-
-  public static function getUser($id){
-    return (new Database('users'))->select('id='.$id)
-    ->fetchObject(self::class);
-  }
-
   public function cadastrar(){
-    $this->data = date('Y-m-d H:i:s');
-    $obDatabase = new Database('users');
-    $this->id = $obDatabase->insert([
-                                        'name' => $this->name,
-                                        'email' => $this->email,
-                                        'cpf' => $this->cpf,
-                                        'telefone' => $this->telefone,
-                                        'status' => $this->status
-                                    ]);
-    return true;                             
+    $database = new Database('users');
+    
+    $this->id = $database->insert([
+      'name' => $this->name,
+      'email' => $this->email,
+      'password' => $this->password,
+      'cpf' => $this->cpf,
+      'phone' => $this->phone,
+      'status' => $this->status
+    ]);
+
+    return true;
   }
 
-  public function atualizar(){
-    return (new Database('users'))->update('id='.$this->id,[
-                                                'name' => $this->name,
-                                                'email' => $this->email,
-                                                'cpf' => $this->cpf,
-                                                'telefone' => $this->telefone,
-                                                'status' => $this->status
-                                              ]);                  
-  }
-
-  public function excluir(){
-    return (new Database('users'))->delete('id = '.$this->id);
-  }
-
-  public static function auth($where=null, $order=null, $limit = null){
-    return (new Database('users'))->select($where, $order, $limit)
-    ->fetchAll(PDO::FETCH_CLASS,self::class);
-  }
 
 }
