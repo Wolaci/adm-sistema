2fc39aa997bab008e4963e2b96dc3fe3c8c23ecf
Wolaci <wolacijose8@gmail.com>
2021-07-19 19:29:31 -0300

    Finalizando formulário de registro

--- includes/formRegister.php
+++ includes/formRegister.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Registration Page</title>
+  <title>Cadastro de Usuário</title>
   <?php require_once('templates/templateChamada.php') ?>
 </head>
 <body class="hold-transition register-page">
@@ -14,7 +14,7 @@
 
   <div class="card">
     <div class="card-body register-card-body">
-      <p class="login-box-msg">Criar um novo usuário</p>
+      <p class="login-box-msg">Cadastrar um novo usuário</p>
 
       <form action="../../index.html" method="post">
         <div class="input-group mb-3">
@@ -34,7 +34,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="text" name="cpf" class="form-control" placeholder="CPF">
+          <input type="text" name="cpf" id="cpf" class="form-control" placeholder="CPF">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -42,7 +42,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="text" name="phone" class="form-control" placeholder="Telefone">
+          <input type="text" name="phone" id="phone" class="form-control" placeholder="Telefone">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-phone"></span>
@@ -68,7 +68,7 @@
         <div class="row">
           <!-- /.col -->
           <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
+            <button type="submit" class="btn btn-primary btn-block">Cadastrar</button>
           </div>
           <!-- /.col -->
         </div>
@@ -80,5 +80,9 @@
   </div><!-- /.card -->
 </div>
 <!-- /.register-box -->
+<script>
+  $('#cpf').mask('000.000.000-00', {reverse: true});
+  $('#phone').mask('(00) 00000-0000');
+</script>
 </body>
 </html>
